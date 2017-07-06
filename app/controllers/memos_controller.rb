8a4bec83ac589346d3fc15f37b6e86eaba4b1fad
Hyeonseok seo <shsvkdldj@likelion.org>
2017-07-06 08:50:19 +0000

    memo controller, new action, View, Tinymce

---
+++ app/controllers/memos_controller.rb
@@ -0,0 +1,23 @@
+class MemosController < ApplicationController
+  def new
+    @memo = Memo.new
+  end
+  
+  def create
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+ 
+
   devise_for :users
   
-  resources :memos 
+  resources :memos #모든 route 끝내는 문장!  
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
